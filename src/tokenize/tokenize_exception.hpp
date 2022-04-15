7df8ee62057f1a783bfdde1a73c7e410595841c8
BeastLe9enD <beastle9end@outlook.de>
2022-04-15 14:41:10 +0200

    ✨ Add string tokenization

---
+++ src/tokenize/tokenize_exception.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../util/text/line_offset.hpp"
+
+#include <fmt/format.h>
+#include <exception>
+
+namespace karmac {
+    class TokenizeException final : public std::exception {
+    private:
+
+    public:
+
+    };
+}
